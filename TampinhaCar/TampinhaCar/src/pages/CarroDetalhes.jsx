5204d0623acfe0faa1d36e4c12a549ddb6260c31
Jhonnatha Castro <Jhonnatha111@gmail.com>
2025-07-27 18:48:46 +0100

    preparando galeria.json

--- TampinhaCar/TampinhaCar/src/pages/CarroDetalhes.jsx
+++ TampinhaCar/TampinhaCar/src/pages/CarroDetalhes.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+
+import Header from '../components/Header';
+
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import "../css/carroDetalhes.css";
 
 function CarroDetalhes() {
   const { id } = useParams();
@@ -8,7 +12,7 @@ function CarroDetalhes() {
   const loading = useSelector((state) => state.carros.loading);
 
   if (loading) {
-    return <p>Carregando carros...</p>;
+    return <p>Carregando ...</p>;
   }
 
   const carro = carros.find(c => c.id == Number(id));
@@ -18,12 +22,56 @@ function CarroDetalhes() {
   }
 
   return (
+    <>
+              <Header />
+    <picture>
+       <h1>Fotos</h1>
+    </picture>
+    <section>
+   <h1>Destaques</h1>
+    </section>
+    <article>
+      <h1>Anotações</h1>
+    </article>
+    <section>
+      <div>
+        <h1>Informaçoes Basicas</h1>
+      </div>
+      <section>
+        <h1>Detalhes</h1>
+        <div>
+          <h2>Especificaçoes tecnicas</h2>
+        </div>
+        <div>
+          <h2>Especificaçoes tecnicas</h2>
+        </div>
+      </section>
+      <section>
+        <h1>Detalhes</h1>
+        <div>
+          <h2>Especificaçoes tecnicas</h2>
+        </div>
+        <div>
+          <h2>Especificaçoes tecnicas</h2>
+        </div>
+        <div>
+          <h2>Especificaçoes tecnicas</h2>
+        </div>
+        <div>
+          <h2>Especificaçoes tecnicas</h2>
+        </div>
+      </section>
+
+
+    </section>
+
     <div>
       <h1>{carro.name}</h1>
       <p>Modelo: {carro.model}</p>
       <p>Ano: {carro.year}</p>
-      {/* Adicione aqui outras infos do carro que quiser mostrar */}
+      {/* Adicione aqui outras infos  */}
     </div>
+    </>
   );
 }
 
